c178766686b382649945886ac89f0c382698963e
Yukai Huang <yukaihuangtw@gmail.com>
2022-04-02 16:18:30 +0800

    feat: display name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { RefreshIcon, SearchIcon } from '@heroicons/react/outline';
 import { useMemo, useState } from 'react';
 import useSWR from 'swr';
 
@@ -15,7 +16,7 @@ async function findInboxFolder(dir: FileSystemDirectoryHandle) {
   }
 }
 
-async function getSubdirs(dir: FileSystemDirectoryHandle) {
+async function getSubDirs(dir: FileSystemDirectoryHandle) {
   const entries: FileSystemDirectoryHandle[] = [];
 
   for await (const entry of dir.values()) {
@@ -45,6 +46,30 @@ async function readMessageJSON(dir: FileSystemDirectoryHandle) {
   }
 }
 
+async function readAutofillInformation(dir: FileSystemDirectoryHandle) {
+  try {
+    const file = await dir.getFileHandle('autofill_information.json');
+
+    return (await file.getFile()).text();
+  } catch (e) {
+    return null;
+  }
+}
+
+async function getMyselfName(
+  dir: FileSystemDirectoryHandle
+): Promise<string | null> {
+  const json = await readAutofillInformation(dir);
+  if (json) {
+    const autofill = JSON.parse(json);
+    return decodeString(
+      autofill['autofill_information_v2']?.['FULL_NAME']?.[0]
+    ) as string;
+  } else {
+    return null;
+  }
+}
+
 const chatCache = new Map<string, string>();
 
 type Chat = {
@@ -61,7 +86,7 @@ async function loadChats(
     return [];
   }
 
-  const subdirs = await getSubdirs(inboxDir);
+  const subdirs = await getSubDirs(inboxDir);
   return Promise.all(
     subdirs.map(async (dir) => {
       const messageJSON = await readMessageJSON(dir);
@@ -95,6 +120,12 @@ export default function HomePage() {
   );
 
   const { data } = useSWR('chats', () => loadChats(inboxDir));
+  const { data: myName = null } = useSWR(
+    () => (directory ? 'myName' : false),
+    () => {
+      return getMyselfName(directory!);
+    }
+  );
 
   const chats = useMemo(() => {
     if (!data || data.length === 0) {
@@ -112,7 +143,7 @@ export default function HomePage() {
 
       if (inbox) {
         setInboxDir(inbox);
-        setDirectory(directory);
+        setDirectory(directoryHandle);
       } else {
         window.alert('This is not a valid Messenger archive folder.');
       }
@@ -138,28 +169,54 @@ export default function HomePage() {
       <div className='flex h-full'>
         {/* Sidebar */}
         <div
-          className='h-full max-h-full overflow-y-auto overflow-x-hidden'
+          className='flex h-full max-h-full flex-col border-r border-solid'
           style={{ maxWidth: 350 }}
         >
-          {chats.map((chat) => (
-            <div className='max-w-full px-1 py-1.5' key={chat.dirName}>
-              <div className='flex cursor-pointer flex-col rounded-lg py-3 px-5 hover:bg-gray-100'>
-                <span className='mb-1 max-w-full overflow-hidden text-ellipsis whitespace-nowrap'>
-                  {chat.title}
-                </span>
-                <small className='max-w-full overflow-hidden text-ellipsis'>
-                  {chat.dirName}
-                </small>
-              </div>
+          <div className='flex flex-col items-start justify-center border-b border-solid px-4 py-4'>
+            <div className='mb-4 flex w-full items-center justify-between'>
+              <h3 className='text-lg font-semibold'>
+                Viewing {myName}&#39;s chat history
+              </h3>
+
+              <button className='rounded-full border-none p-2 hover:bg-gray-100'>
+                <RefreshIcon width={18} className='cursor-pointer' />
+              </button>
             </div>
-          ))}
+
+            <label className='relative w-full flex-1'>
+              <input
+                placeholder='Search for user...'
+                className='w-full rounded-lg border-none bg-gray-100 py-2 px-4 pl-8 text-sm text-gray-500 outline-none ring-1 ring-gray-100 focus:ring-2 focus:ring-blue-500'
+              />
+              <SearchIcon
+                className='absolute top-0 left-2.5 h-full stroke-gray-500'
+                width={16}
+                height={16}
+              />
+            </label>
+          </div>
+
+          <div className='overflow-y-auto overflow-x-hidden'>
+            {chats.map((chat) => (
+              <div className='max-w-full px-1 py-1.5' key={chat.dirName}>
+                <div className='flex cursor-pointer flex-col rounded-lg py-3 px-5 hover:bg-gray-100'>
+                  <span className='mb-1 max-w-full overflow-hidden text-ellipsis whitespace-nowrap'>
+                    {chat.title}
+                  </span>
+                  <small className='max-w-full overflow-hidden text-ellipsis'>
+                    {chat.dirName}
+                  </small>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
 
         {/* Message boxes */}
         <div className='flex flex-1 flex-col'>
           <div className='flex h-14 w-full items-center'>Nav</div>
 
-          <div className='flex-1 overflow-y-auto'>Content</div>
+          <div className='flex-1 overflow-y-auto'></div>
         </div>
       </div>
     );
